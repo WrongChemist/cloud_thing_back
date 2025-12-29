2989708e8ae58466b34a58cef0315f4154e86763
WrongChemist <161587646+WrongChemist@users.noreply.github.com>
2025-12-29 12:12:53 +0200

    express fix

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import { Express } from 'express';
+import express from 'express';
 
 // const express = require('express')
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send({'status': 'alive'});
